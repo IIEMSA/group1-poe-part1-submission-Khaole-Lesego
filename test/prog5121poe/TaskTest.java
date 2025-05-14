0155968708052a1a3a452d23289211161f5b0b60
LesegoKhaole <ST10455441@imconnect.edu.za>
2025-05-14 04:42:33 +0200

    TaskTest started
    The Task File has been completed, and unit tests are in progress

---
+++ test/prog5121poe/TaskTest.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package prog5121poe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author leseg
+ */
+public class TaskTest {
+    
+    public TaskTest() {
+    }
+
+    @Test
+    public void testCheckTaskDescription() {
+    }
+
+    @Test
+    public void testCreateTaskID() {
+    }
+
+    @Test
+    public void testPrintTaskDetails() {
+    }
+
+    @Test
+    public void testGetTaskDuration() {
+    }
+    
+}
